5347e5570a2801b728213c71c94659e510b811da
Esubalew197 <esubalewmillion@gmail.com>
2023-12-05 22:37:18 +0300

    Task  6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * function that changes array to set
+ * @param {[*]} arr - Takes in array of any type
+ * @returns a set
+ */
+export default function setFromArray(arr) {
+  return new Set(arr);
+}
